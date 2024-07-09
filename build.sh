75a01eb4d6c2b851c569cc7b63cad3dad848344a
Alex Baranowski <alex@euro-linux.com>
2024-07-09 12:03:22 +0000

    small changes to build script

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+set -x
+
 my_arch=$(arch)
-podman rm el9
+rm -f el9-${my_arch}.tar el9-${my_arch}.tar.gz
+podman rm -f el9
 podman run -i --name el9 docker.io/eurolinux/eurolinux-9:latest < install-script.sh
 podman export el9 > "el9-${my_arch}.tar"
 tar -czf - "el9-${my_arch}.tar" | gzip > "el9-${my_arch}.tar.gz"
+set +x
+
+echo "Generation finished! WSL images saved: el9-${my_arch}.tar.gz el9-${my_arch}.tar"
